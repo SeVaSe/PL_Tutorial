7f4cdeeb82218d51fc0ee3ae899b288fefd46c0e
sevase <kotaseva8@gmail.com>
2023-05-11 21:30:39 +0300

    ессссссссссстьььььььььььььььььььььь 7 часов и баг исправлен с поиском мммм, кайф

--- PL_Tutorial/Scroll_Class.cs
+++ PL_Tutorial/Scroll_Class.cs
@@ -20,17 +20,37 @@ namespace PL_Tutorial.Window_Menu
         {
             if (e.Key == Key.Enter)
             {
-                string searchText = Txt_Box_Search.Text.ToLower();
+                string searchText = Txt_Box_Search.Text.ToLower(); // получаем введенный текст из TextBox и приводим к нижнему регистру
 
+                // снимаем выделение и изменяем фоновый цвет с предыдущего найденного элемента
                 if (lastFoundItem != null)
                 {
                     lastFoundItem.IsSelected = false;
                     lastFoundItem.Background = Brushes.Red;
                 }
 
-                bool itemFound = false;
+                bool flagItemFound = false;
 
                 foreach (var item in Lst_Box_Osn.Items)
+                {
+                    ListBoxItem listBoxItem = item as ListBoxItem;
+                    if (listBoxItem != null && listBoxItem.Content.ToString().ToLower().Contains(searchText))
+                    {
+                        listBoxItem.IsSelected = true; // выделяем найденный элемент в ListBox
+                        listBoxItem.Background = Brushes.Yellow; // устанавливаем желтый цвет фона для найденного элемента
+                        listBoxItem.Focus(); // устанавливаем фокус на найденный элемент, чтобы обновить его внешний вид
+                        Lst_Box_Osn.ScrollIntoView(listBoxItem); // прокручиваем ListBox к найденному элементу
+                        lastFoundItem = listBoxItem; // сохраняем ссылку на найденный элемент
+                        Txt_Box_Search.Focus(); // устанавливаем фокус на Txt_Box_Search
+
+                        flagItemFound = true;
+
+                        break;
+                    }
+                }
+
+               
+                foreach (var item in Lst_OOP.Items)
                 {
                     ListBoxItem listBoxItem = item as ListBoxItem;
                     if (listBoxItem != null && listBoxItem.Content.ToString().ToLower().Contains(searchText))
@@ -38,46 +58,25 @@ namespace PL_Tutorial.Window_Menu
                         listBoxItem.IsSelected = true;
                         listBoxItem.Background = Brushes.Yellow;
                         listBoxItem.Focus();
-                        Lst_Box_Osn.ScrollIntoView(listBoxItem);
+                        Lst_OOP.ScrollIntoView(listBoxItem);
                         lastFoundItem = listBoxItem;
-                        Txt_Box_Search.Focus();
+                        Txt_Box_Search.Focus(); // устанавливаем фокус на Txt_Box_Search
 
-                        itemFound = true;
+                        flagItemFound = true;
 
                         break;
                     }
                 }
 
-                if (!itemFound)
+                if (flagItemFound == false)
                 {
-                    foreach (var item in Lst_OOP.Items)
-                    {
-                        ListBoxItem listBoxItem = item as ListBoxItem;
-                        if (listBoxItem != null && listBoxItem.Content.ToString().ToLower().Contains(searchText))
-                        {
-                            listBoxItem.IsSelected = true;
-                            listBoxItem.Background = Brushes.Yellow;
-                            listBoxItem.Focus();
-                            Lst_OOP.ScrollIntoView(listBoxItem);
-                            lastFoundItem = listBoxItem;
-                            Txt_Box_Search.Focus();
-
-                            itemFound = true;
-
-                            break;
-                        }
-                    }
+                    Txt_Box_Search.Clear();
                 }
-
-                if (!itemFound)
-                {
-                    // Обработка случая, когда элемент не был найден
-                    // ...
-                }
+                
             }
         }
 
-
+        
 
 
 
