0ad8829ea970a99fe904db2457e6502ebe77df4c
sevase <108822198+SeVaSe@users.noreply.github.com>
2023-06-01 22:50:38 +0300

    сделано окно сообщения об ошибке и об предупреждении

---
+++ PL_Tutorial/Dop_Window/MessageBoxWarn_Modern.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PL_Tutorial.Dop_Window
+{
+    /// <summary>
+    /// Логика взаимодействия для MessageBoxWarn_Modern.xaml
+    /// </summary>
+    public partial class MessageBoxWarn_Modern : Window
+    {
+        public MessageBoxWarn_Modern()
+        {
+            InitializeComponent();
+        }
+
+        //закрытие
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+
+
+
+    }
+}
